358fc2a346d7871cd30a8edc245992fafadba9c9
Leandro Mora <leandro.mora@globant.com>
2013-07-30 12:29:50 -0300

    extracted js

---
+++ imageGetter1.js
@@ -0,0 +1,14 @@
+var participant = null;
+participant = gapi.hangout.getLocalParticipant();
+var para=document.createElement("p");
+var node=document.createTextNode(participant.person.id);
+para.appendChild(node);
+var element=document.getElementById("div2");
+element.appendChild(para);          
+var canvas = document.getElementById("e");
+var context = canvas.getContext("2d");
+var cat = new Image();
+cat.src = "http://hangoutmediastarter.appspot.com/static/mustache.png";
+cat.onload = function() {
+  context.drawImage(cat, 0, 0);
+};
